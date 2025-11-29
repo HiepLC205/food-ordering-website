bc8af9efd660281cae762399cf8c586bbd70df4e
Nong Thi Hoai <Hoails3007@gmail.com>
2025-11-29 20:36:32 +0700

    Register modal

--- main.js
+++ main.js
@@ -22,37 +22,65 @@ document.addEventListener("click", (e) => {
     }
   }
 });
-// phần đăng nhập
-const login = document.getElementById("login_on");
-const regester = document.getElementById("regester_on");
-const btnlogin = document.querySelectorAll(".btn_login");
-const btnregester = document.querySelectorAll(".btn_regester");
-const close_login = document.querySelector(".model_close-btn");
-const menulogin = document.querySelector(".model_overlay");
-console.log(btnlogin);
-console.log(btnregester);
-console.log(close_login);
-// nut dang nhap
-btnlogin.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    login.style.display = "block";
-    regester.style.display = "none";
-    menulogin.style.display = "flex";
-    navList.style.display = "none";
-  });
+// mở modal đăng kí
+function openRegister() {
+  const modal = document.getElementById("register-modal");
+  if (modal) {
+    modal.style.display = "flex";
+  } else {
+    console.error("Lỗi: Không tìm thấy modal ID 'register-modal'");
+  }
+}
+// modal đăng kí
+const registerForm = document.getElementById("registerForm");
+registerForm.addEventListener("submit", function (check) {
+  check.preventDefault();
+  const email = document.getElementById("email").value;
+  const pwd = document.getElementById("pwd").value;
+  const cf_pwd = document.getElementById("cf_pwd").value;
+
+  const check_email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!check_email.test(email)) {
+    alert("Email không đúng định dạng. Ví dụ: abc@gmail.com");
+    return;
+  }
+  if (pwd !== cf_pwd) {
+    alert("Mật khẩu không khớp!");
+    return;
+  }
+  if (pwd.length < 8) {
+    alert("Mật khẩu quá ngắn! Vui lòng nhập ít nhất 8 ký tự.");
+    return;
+  }
+
+  // test demo
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const exists = users.find((u) => u.email == email);
+  if (exists) {
+    alert("Email đã được đăng ký.");
+    return;
+  }
+  alert(`Đăng ký thành công.`);
+  closeModal();
+  const userName = email.split("@")[0];
+  const newUser = { email: email, username: userName, password: pwd };
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+  localStorage.setItem("currentUser", JSON.stringify(newUser));
 });
-// nut dang ky
-btnregester.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    regester.style.display = "block";
-    login.style.display = "none";
-    menulogin.style.display = "flex";
-    navList.style.display = "none";
-  });
-});
-// nut bo
-close_login.addEventListener("click", function (e) {
-  login.style.display = "none";
-  login.style.display = "none";
-  menulogin.style.display = "none";
-});
+console.log(JSON.parse(localStorage.getItem("users")));
+
+// Tắt modal
+function closeModal() {
+  const modal = document.getElementById("register-modal");
+  if (modal) {
+    modal.style.display = "none";
+  }
+}
+// Click ra ngoài để tắt modal
+window.onclick = function (close) {
+  const modal = document.getElementById("register-modal");
+  if (close.target == modal) {
+    modal.style.display = "none";
+  }
+};
